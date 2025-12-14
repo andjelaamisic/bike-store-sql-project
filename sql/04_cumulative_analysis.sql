57c6ce108d2c85101e87b7313941a6a26aaded54
Andjela Misic <andjelaamisic@gmail.com>
2025-12-14 22:25:01 +0100

    Add files via upload

---
+++ sql/04_cumulative_analysis.sql
@@ -0,0 +1,16 @@
+select 
+	order_date,
+	total_sales,
+	sum(total_sales) over (partition by year(order_date) order by order_date) as running_total_sales,
+	avg_price,
+	avg(avg_price) over (partition by year(order_date) order by order_date) as moving_average_price
+from(select
+	datetrunc(month,o.order_date) order_date,
+	cast(sum(s.quantity*s.list_price*(1-s.discount))as int) as total_sales,
+	cast(avg(s.list_price)as int) as avg_price
+from sales.order_items as s
+left join sales.orders as o
+on s.order_id=o.order_id
+where datetrunc(month,o.order_date) is not null
+group by datetrunc(month,o.order_date)
+)t
